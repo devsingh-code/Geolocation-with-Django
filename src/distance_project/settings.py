20e2dfe21b7ae935161c9e4ad15a1fad0acd0fc8
Deovrat Singh <devsingh6n@gmail.com>
2020-08-29 22:31:26 +0530

    Settings setup

--- src/distance_project/settings.py
+++ src/distance_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q5at(&fmn9gp02h%)p_eo8zpvx6&%w*k$8_2wv%jaq9b#krf5c'
+SECRET_KEY = os.environ('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
