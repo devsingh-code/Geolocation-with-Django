810e28c5d8907f478973831e328ff1a9275bf7e7
Deovrat Singh <devsingh6n@gmail.com>
2020-08-30 14:52:07 +0530

    Setting distance caluclation and location markers

--- src/measurements/utils.py
+++ src/measurements/utils.py
@@ -9,3 +9,25 @@ def get_geo(ip):
     lat,lon = g.lat_lon(ip)
 
     return country,city,lat,lon
+
+def get_center_coordinates(latA, lonA,latB=None,lonB=None):
+    cord = (latA, lonA)
+    if latB:
+        cord =[(latA+latB)/2, (lonA+lonB)/2]
+    return cord
+
+def get_zoom(distance):
+    if distance <=100:
+        return 8
+    elif distance>100 and distance<5000:
+        return 4
+    else:
+        return 2
+
+def get_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
--- src/measurements/views.py
+++ src/measurements/views.py
@@ -3,46 +3,71 @@ from .models import Measurement
 from .forms import MeasurementModelForm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
-from .utils import get_geo
-
+from .utils import get_geo, get_center_coordinates,get_zoom, get_ip_address
+import folium
 # Create your views here.
 def calculate_distance_view(request):
+    #initial values
+    distance = None
+    destination=None
+
     obj = get_object_or_404(Measurement, id=1)
     form = MeasurementModelForm(request.POST or None)
     geolocator = Nominatim(user_agent = 'meausrements')
+    '''this would be used when going live'''
+    #ip = get_ip_address(request)
     ip = '72.14.207.99'
     country, city,lat, lon = get_geo(ip)
-    # print('Location country ', country)
-    # print('Location city', city)
-    # print('Location lat long ', lat, lon)
-
     location =geolocator.geocode(city)
-    #print('###', location)
 
+    #location coordinates
     l_lat = lat
     l_lon = lon
-
     pointA = (l_lat, l_lon)
 
    
+    #initial folium map
+    m = folium.Map(width=1200, height= 800, location=get_center_coordinates(l_lat,l_lon), zoom_start=8)
+
+    #location marker
+    folium.Marker([l_lat, l_lon], tooltip='Click here for more', popup =city['city'],
+            icon =folium.Icon(color='purple')).add_to(m)
 
     if form.is_valid():
         instance = form.save(commit=False)
         destination_ = form.cleaned_data.get('destination')
         destination = geolocator.geocode(destination_)
-        #print(destination)
+        #destination coordinates
         d_lat = destination.latitude
         d_lon = destination.longitude
 
         pointB= (d_lat, d_lon)
         distance = round(geodesic(pointA,pointB).km,2)
+
+        #folum map modification
+        m = folium.Map(width=1200, height= 800, location=get_center_coordinates(l_lat,l_lon, d_lat, d_lon), zoom_start=get_zoom(distance))
+        folium.Marker([l_lat, l_lon], tooltip='Click here for more', popup =city['city'],
+            icon =folium.Icon(color='purple')).add_to(m)
+        #destination marker    
+        folium.Marker([d_lat, d_lon], tooltip='Click here for more', popup =destination,
+            icon =folium.Icon(color='red', icon='cloud')).add_to(m)
+
+        #draw line between location and destination
+        line = folium.PolyLine(locations=[pointA, pointB], weight=2, color='blue')
+        m.add_child(line)
+
         instance.location = 'Goa'
         instance.distance = distance
         instance.save()
+    
+    m = m._repr_html_()
+    
 
     context = {
-        'distance': obj,
+        'distance': distance,
+        'destination':destination,
         'form':form,
+        'map':m,
     }
 
     return render(request, 'measurements/main.html', context)
