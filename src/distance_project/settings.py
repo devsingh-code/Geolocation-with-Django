1a20941630a5052276beb019afd58436133f143e
Deovrat Singh <devsingh6n@gmail.com>
2020-08-30 12:19:46 +0530

    Setting geopy and geoip2 logic

--- src/distance_project/settings.py
+++ src/distance_project/settings.py
@@ -126,3 +126,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK ='bootstrap4'
+
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
--- src/measurements/views.py
+++ src/measurements/views.py
@@ -1,17 +1,43 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Measurement
 from .forms import MeasurementModelForm
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+from .utils import get_geo
 
 # Create your views here.
 def calculate_distance_view(request):
     obj = get_object_or_404(Measurement, id=1)
     form = MeasurementModelForm(request.POST or None)
+    geolocator = Nominatim(user_agent = 'meausrements')
+    ip = '72.14.207.99'
+    country, city,lat, lon = get_geo(ip)
+    # print('Location country ', country)
+    # print('Location city', city)
+    # print('Location lat long ', lat, lon)
+
+    location =geolocator.geocode(city)
+    #print('###', location)
+
+    l_lat = lat
+    l_lon = lon
+
+    pointA = (l_lat, l_lon)
+
+   
 
     if form.is_valid():
         instance = form.save(commit=False)
-        instance.destination = form.cleaned_data.get('destination')
+        destination_ = form.cleaned_data.get('destination')
+        destination = geolocator.geocode(destination_)
+        #print(destination)
+        d_lat = destination.latitude
+        d_lon = destination.longitude
+
+        pointB= (d_lat, d_lon)
+        distance = round(geodesic(pointA,pointB).km,2)
         instance.location = 'Goa'
-        instance.distance = 5000.00
+        instance.distance = distance
         instance.save()
 
     context = {
